dd9e9d01b002b2256aa392b6a6f6abfc722ccc42
Juan Felipe Hurtado <63176364+Skpow1234@users.noreply.github.com>
2020-10-15 20:30:33 -0500

    Carga inicial

---
+++ Dropbox/Mi PC (DESKTOP-CU6IIF6)/Desktop/appfluttercart/lib/pages/login/widgets/register_form.dart
@@ -0,0 +1,216 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_cart_app/libs/auth.dart';
+import 'package:flutter_cart_app/pages/home/home_page.dart';
+import 'package:flutter_cart_app/pages/login/widgets/input_text_login.dart';
+import 'package:flutter_cart_app/utils/app_colors.dart';
+import 'package:flutter_cart_app/utils/dialogs.dart';
+import 'package:flutter_cart_app/utils/extras.dart';
+import 'package:flutter_cart_app/utils/responsive.dart';
+import 'package:flutter_cart_app/widgets/rounded_button.dart';
+
+class RegisterForm extends StatefulWidget {
+  final VoidCallback onGoToLogin;
+  final Alignment alignment;
+  const RegisterForm(
+      {Key key,
+      @required this.onGoToLogin,
+      this.alignment = Alignment.bottomCenter})
+      : super(key: key);
+
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  bool _agree = false;
+
+  final GlobalKey<InputTextLoginState> _usernameKey = GlobalKey();
+  final GlobalKey<InputTextLoginState> _emailKey = GlobalKey();
+  final GlobalKey<InputTextLoginState> _passwordKey = GlobalKey();
+  final GlobalKey<InputTextLoginState> _vpasswordKey = GlobalKey();
+
+  void _goTo(User user) {
+    if (user != null) {
+      Navigator.pushReplacementNamed(context, HomePage.routeName);
+    } else {
+      print("Registro fallido");
+    }
+  }
+
+  _submit() async {
+    final String username = _usernameKey.currentState.value;
+    final String email = _emailKey.currentState.value;
+    final String password = _passwordKey.currentState.value;
+    final String vpassword = _vpasswordKey.currentState.value;
+
+    final bool usernameOk = _usernameKey.currentState.isOk;
+    final bool emailOk = _emailKey.currentState.isOk;
+    final bool passwordOk = _passwordKey.currentState.isOk;
+    final bool vpasswordOk = _vpasswordKey.currentState.isOk;
+
+    if (usernameOk && emailOk && passwordOk && vpasswordOk) {
+      if (_agree) {
+        final User user = await Auth.instance.signUp(
+          context,
+          username: username,
+          email: email,
+          password: password,
+        );
+
+        _goTo(user);
+      } else {
+        Dialogs.alert(context,
+            description: "you need to accept the terms and conditions");
+      }
+    } else {
+      Dialogs.alert(context, description: "Algunos campos invalidos");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Responsive responsive = Responsive.of(context);
+
+    return Align(
+      alignment: widget.alignment,
+      child: SafeArea(
+        top: false,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          constraints: BoxConstraints(maxWidth: 400),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                "Nueva cuenta",
+                style: TextStyle(
+                    color: AppColors.primary,
+                    fontSize: 30,
+                    fontFamily: 'raleway',
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "En Cart buscamos su comodidad.",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              SizedBox(
+                height: responsive.ip(2),
+              ),
+              InputTextLogin(
+                key: _usernameKey,
+                iconPath: 'assets/pages/login/icons/avatar.svg',
+                placeholder: "Usuario",
+                validator: (text) {
+                  return text.trim().length > 0;
+                },
+              ),
+              SizedBox(
+                height: responsive.ip(2),
+              ),
+              InputTextLogin(
+                key: _emailKey,
+                iconPath: 'assets/pages/login/icons/email.svg',
+                placeholder: "Direccion correo",
+                keyboardType: TextInputType.emailAddress,
+                validator: (text) => Extras.isValidEmail(text),
+              ),
+              SizedBox(
+                height: responsive.ip(2),
+              ),
+              InputTextLogin(
+                key: _passwordKey,
+                iconPath: 'assets/pages/login/icons/key.svg',
+                placeholder: "Contraseña",
+                obscureText: true,
+                validator: (text) {
+                  _vpasswordKey.currentState?.checkValidation();
+                  return text.trim().length >= 6;
+                },
+              ),
+              SizedBox(
+                height: responsive.ip(2),
+              ),
+              InputTextLogin(
+                key: _vpasswordKey,
+                obscureText: true,
+                iconPath: 'assets/pages/login/icons/key.svg',
+                placeholder: "Confirme contraseña",
+                validator: (text) {
+                  return text.trim().length >= 6 &&
+                      _vpasswordKey.currentState.value ==
+                          _passwordKey.currentState.value;
+                },
+              ),
+              SizedBox(
+                height: responsive.ip(2),
+              ),
+              DefaultTextStyle(
+                style: TextStyle(
+                    fontSize: responsive.ip(1.3),
+                    color: Theme.of(context).textTheme.subtitle2.color),
+                child: Wrap(
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: <Widget>[
+                    Checkbox(
+                      value: _agree,
+                      onChanged: (isChecked) {
+                        setState(() {
+                          _agree = isChecked;
+                        });
+                      },
+                    ),
+                    Text("Acepto los "),
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        "terminos",
+                        style: TextStyle(
+                            color: AppColors.primary,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(" y las "),
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        "politicas",
+                        style: TextStyle(
+                            color: AppColors.primary,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: responsive.ip(2),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: widget.onGoToLogin,
+                    child: Text("← Volver a Log In"),
+                  ),
+                  SizedBox(width: 10),
+                  RoundedButton(
+                    label: "Registrate",
+                    onPressed: _submit,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: responsive.ip(2),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
